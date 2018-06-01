b52194288d26a36a6619e426a5dbc42949afc4ca
Ashwani <aksinghal1125@gmail.com>
2018-05-31 20:30:22 -0500

    M

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/Home/Home';
+import App from './Home/Home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
